32c4500e9c683bc3b4a12f6d78c6649d0b880d94
James <jamesdev4you@gmail.com>
2023-06-12 16:16:23 -0400

    yo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./primary_components/Menu";
+import Home from "./primary_components/Home";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  shape: {
+    pillRadius: 50,
+  },
+  palette: {
+    primary: { main: "#000000" },
+    secondary: {
+      main: "#fff",
+    },
+    error: { main: "#ffd700" },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Menu />
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/primary_components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Background from "../assets/background.jpg";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
